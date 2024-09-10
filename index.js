a25ee8a3aabc5958db5440d8dcb56050ef947bf9
cheneyweb <457299596@qq.com>
2024-09-10 15:01:34 +0800

    1.0

--- index.js
+++ index.js
@@ -5,26 +5,23 @@ const { koaBody } = require('koa-body')
 const router = new Router()
 
 router.post('/', async (ctx) => {
-    const { ToUserName, FromUserName, MsgType, Content, CreateTime, action } = ctx.request.body
+    const { ToUserName, FromUserName, MsgType, Content, CreateTime } = ctx.request.body
 
     console.log(ToUserName)
     console.log(FromUserName)
     console.log(MsgType)
     console.log(Content)
     console.log(CreateTime)
-    console.log(action)
 
-    ctx.body = 'success'
-
-    // if (MsgType === 'text') {
-    //     ctx.body = {
-    //         ToUserName: FromUserName,
-    //         FromUserName: ToUserName,
-    //         CreateTime: CreateTime,
-    //         MsgType: 'text',
-    //         Content: '这是回复的消息'
-    //     }
-    // }
+    if (MsgType === 'text') {
+        ctx.body = {
+            ToUserName: FromUserName,
+            FromUserName: ToUserName,
+            CreateTime: CreateTime,
+            MsgType: 'text',
+            Content: '这是回复的消息'
+        }
+    }
 })
 
 const app = new Koa()
