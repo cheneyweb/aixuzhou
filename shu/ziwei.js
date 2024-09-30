ab0196df65049a9e2b54f13b8c4cab99d0d643f7
cheneyweb <457299596@qq.com>
2024-09-30 14:46:54 +0800

    1.0

--- shu/ziwei.js
+++ shu/ziwei.js
@@ -28,8 +28,8 @@ function getSiHuas(life) {
     // 串联 {化:[离心四化/向心四化]}
     const liXinChuanLianMap = {}
     const xiangXinChuanLianMap = {}
-    // 并联 {化化/化化化/化化化化:[宫,宫]}
-    const bingLianMap = {}
+    // 并联 {}
+    const bingLianMap = { shengnianshuangxiang: {}, shengniandanxiangzihua: {}, chuanbing: {}, dangongshuangxiang: {} }
     // 反背 {化:{'离心':[离心四化],'向心':[向心四化]}}
     const fanBeiMap = {}
 
@@ -58,7 +58,7 @@ function getSiHuas(life) {
     }
 
     // 遍历每宫
-    for (palaceFrom of life.palaces) {
+    for (let palaceFrom of life.palaces) {
         // 建立地支宫位索引MAP
         diZhiGongMap[palaceFrom.earthlyBranch] = palaceFrom
         // 合并每宫主辅星耀
@@ -69,11 +69,13 @@ function getSiHuas(life) {
         // 宫位男女星
         gongNanNvMap[palaceFrom.name] = []
 
+        bingLianMap.shengnianshuangxiang[palaceFrom.name] = []
         // 遍历每宫星耀
         for (let star of starFroms) {
             // 生年四化
             if (star.mutagen) {
                 shengNianSiHuas.push([palaceFrom.name, star.name, star.mutagen])
+                bingLianMap.shengnianshuangxiang[palaceFrom.name].push([palaceFrom.name, star.name, star.mutagen])
             }
             // 离心四化
             const starSiHua = ziwei.SIHUAMAP[palaceFrom.heavenlyStem][star.name]
@@ -98,6 +100,10 @@ function getSiHuas(life) {
 
         // 遍历对宫星耀
         for (let star of starTos) {
+            // 生年四化
+            if (star.mutagen) {
+                bingLianMap.shengnianshuangxiang[palaceFrom.name].push([palaceTo.name, star.name, star.mutagen])
+            }
             // 向心四化
             const starSiHua = ziwei.SIHUAMAP[palaceFrom.heavenlyStem][star.name]
             if (starSiHua) {
@@ -195,124 +201,32 @@ function getSiHuas(life) {
         }
     }
 
-    return { shengNianSiHuas, liXinSiHuas, xiangXinSiHuas, feiGongSiHuaMap, sanHeGongMap, siZhengGongMap, gongNanNvMap, sanHePos, siZhengPos, liXinChuanLianMap, xiangXinChuanLianMap, fanBeiMap }
+    // 遍历生年本对双象
+    for (let gong in bingLianMap.shengnianshuangxiang) {
+        // 删除重复对宫
+        delete bingLianMap.shengnianshuangxiang[ziwei.DUIGONGMAP[gong]]
+        // 删除非双象
+        if (bingLianMap.shengnianshuangxiang[gong].length < 2) {
+            delete bingLianMap.shengnianshuangxiang[gong]
+        }
+    }
+
+    return {
+        shengNianSiHuas,
+        liXinSiHuas,
+        xiangXinSiHuas,
+        feiGongSiHuaMap,
+        sanHeGongMap,
+        siZhengGongMap,
+        gongNanNvMap,
+        sanHePos,
+        siZhengPos,
+        liXinChuanLianMap,
+        xiangXinChuanLianMap,
+        fanBeiMap,
+        bingLianMap
+    }
 }
-
-// // 生年四化 [宫,星,化]
-// function getShengNianSiHuas(life) {
-//     const shengNianSiHuas = []
-//     for (palace of life.palaces) {
-//         for (let star of palace.majorStars.concat(palace.minorStars)) {
-//             if (star.mutagen) {
-//                 shengNianSiHuas.push([palace.name, star.name, star.mutagen])
-//             }
-//         }
-//     }
-//     return shengNianSiHuas
-// }
-// // 离心四化 [宫,星,化]
-// function getLiXinSiHuas(life) {
-//     const liXinSiHuas = []
-//     for (let gong of ziwei.GONGS) {
-//         const palace = life.palace(gong)
-//         for (let star of palace.majorStars.concat(palace.minorStars)) {
-//             const starSiHua = ziwei.SIHUAMAP[palace.heavenlyStem][star.name]
-//             if (starSiHua) {
-//                 liXinSiHuas.push([gong, star.name, starSiHua])
-//             }
-//         }
-//     }
-//     return liXinSiHuas
-// }
-// // 向心四化 [来源宫,目的宫,星,化]
-// function getXiangXinSiHuas(life) {
-//     const xiangXinSiHuas = []
-//     for (let gongFrom of ziwei.GONGS) {
-//         const palaceFrom = life.palace(gongFrom)
-//         const palaceTo = life.palace(ziwei.DUIGONGMAP[gongFrom])
-
-//         for (let star of palaceTo.majorStars.concat(palaceTo.minorStars)) {
-//             const starSiHua = ziwei.SIHUAMAP[palaceFrom.heavenlyStem][star.name]
-//             if (starSiHua) {
-//                 xiangXinSiHuas.push([gongFrom, palaceTo.name, star.name, starSiHua])
-//             }
-//         }
-//     }
-//     return xiangXinSiHuas
-// }
-// // 飞宫四化 {来源宫:[目的宫,星,化]}
-// function getFeiGongSiHuaMap(life) {
-//     const feiGongSiHuaMap = {}
-//     for (let gongFrom of ziwei.GONGS) {
-//         feiGongSiHuaMap[gongFrom] = []
-//         for (let gongTo of ziwei.GONGS) {
-//             const palaceFrom = life.palace(gongFrom)
-//             const palaceTo = life.palace(gongTo)
-//             for (let star of palaceTo.majorStars.concat(palaceTo.minorStars)) {
-//                 const starSiHua = ziwei.SIHUAMAP[palaceFrom.heavenlyStem][star.name]
-//                 if (starSiHua) {
-//                     feiGongSiHuaMap[gongFrom].push([gongTo, star.name, starSiHua])
-//                 }
-//             }
-//         }
-//     }
-//     return feiGongSiHuaMap
-// }
-// // 三合宫位 {三合:[宫,宫,宫]}
-// function getSanHeGongMap(life) {
-//     const sanHeGongMap = {}
-//     for (let sanhe of ziwei.SANHES) {
-//         sanHeGongMap[sanhe] = []
-//         for (let i = 0; i < 3; i++) {
-//             for (palace of life.palaces) {
-//                 if (sanhe[i] === palace.earthlyBranch) {
-//                     sanHeGongMap[sanhe].push(palace.name)
-//                 }
-//             }
-//         }
-//     }
-//     return sanHeGongMap
-// }
-
-// 四正宫位 {四正:[宫,宫,宫,宫]}
-// function getSiZhengGongMap(life) {
-//     const siZhengGongMap = {}
-//     for (let sizheng of ziwei.SIZHENGS) {
-//         siZhengGongMap[sizheng] = []
-//         for (let i = 0; i < 4; i++) {
-//             for (palace of life.palaces) {
-//                 if (sizheng[i] === palace.earthlyBranch) {
-//                     siZhengGongMap[sizheng].push(palace.name)
-//                 }
-//             }
-//         }
-//     }
-//     return siZhengGongMap
-// }
-
-// 宫位男女星 {宫:[[星,男/女]]}
-// function getGongNanNvMap(life) {
-//     const gongNanNvMap = {}
-//     for (let gong of ziwei.GONGS) {
-//         gongNanNvMap[gong] = []
-//         const palace = life.palace(gong)
-//         for (let star of palace.majorStars.concat(palace.minorStars)) {
-//             if (ziwei.NANXINGS.includes(star.name)) {
-//                 gongNanNvMap[gong].push([star.name, '男'])
-//             } else if (ziwei.NVXINGS.includes(star.name)) {
-//                 gongNanNvMap[gong].push([star.name, '女'])
-//             }
-//             if (star.name === '廉贞' && star.withMutagen('禄')) {
-//                 gongNanNvMap[gong].push([star.name, '男'])
-//             } else if (star.name === '廉贞' && star.withMutagen('忌')) {
-//                 gongNanNvMap[gong].push([star.name, '女'])
-//             } else if (star.name === '廉贞') {
-//                 gongNanNvMap[gong].push([star.name, '禄男/忌女'])
-//             }
-//         }
-//     }
-//     return gongNanNvMap
-// }
 
 function getRes(Content) {
     let res = ''
@@ -330,7 +244,7 @@ function getRes(Content) {
         return res = '【输入格式错误】，格式如下：\n紫微 阳历年月日 时辰 性别\n\n【参考如下举例】\n紫微 19990821 申时 男'
     }
 
-    const { shengNianSiHuas, liXinSiHuas, xiangXinSiHuas, feiGongSiHuaMap, sanHeGongMap, siZhengGongMap, gongNanNvMap, sanHePos, siZhengPos, liXinChuanLianMap, xiangXinChuanLianMap, fanBeiMap } = getSiHuas(life)
+    const { shengNianSiHuas, liXinSiHuas, xiangXinSiHuas, feiGongSiHuaMap, sanHeGongMap, siZhengGongMap, gongNanNvMap, sanHePos, siZhengPos, liXinChuanLianMap, xiangXinChuanLianMap, fanBeiMap, bingLianMap } = getSiHuas(life)
     res += `紫微解盘-${life.gender === '男' ? '乾' : '坤'}造`
     res += `\n${life.chineseDate[0]} ${life.chineseDate[3]} ${life.chineseDate[6]} ${life.chineseDate[9]}`
     res += `\n${life.chineseDate[1]} ${life.chineseDate[4]} ${life.chineseDate[7]} ${life.chineseDate[10]}`
@@ -371,6 +285,19 @@ function getRes(Content) {
         res += `${xiangXinChuanLianMap[item][0][3]} → `
         for (let arr of xiangXinChuanLianMap[item]) {
             res += `${arr[1]} `
+        }
+    }
+
+    res += `\n\n【并联】\n`
+    for (let item in bingLianMap.shengnianshuangxiang) {
+        for (let i = 0; i < bingLianMap.shengnianshuangxiang[item].length; i++) {
+            const arr = bingLianMap.shengnianshuangxiang[item][i]
+            res += `${arr[0]} ${arr[1]} ${arr[2]}`
+            if (i < bingLianMap.shengnianshuangxiang[item].length - 1) {
+                res += ` | `
+            } else {
+                res += `\n`
+            }
         }
     }
 
@@ -421,6 +348,7 @@ function getRes(Content) {
     // console.log(liXinChuanLianMap)
     // console.log(xiangXinChuanLianMap)
     // console.log(JSON.stringify(fanBeiMap))
+    // console.log(bingLianMap.shengnianshuangxiang)
 
     return res
 }
